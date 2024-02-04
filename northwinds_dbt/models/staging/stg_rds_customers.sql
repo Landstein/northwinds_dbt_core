4777900fd62c233afa3c72fae4662a48427c59c3
Eric Landstein <eric@landsteinmail.com>
2024-02-04 16:01:41 -0500

    updated models

--- northwinds_dbt/models/staging/stg_rds_customers.sql
+++ northwinds_dbt/models/staging/stg_rds_customers.sql
@@ -1,12 +1,29 @@
-with source as (
-    select * from public.customers
+WITH customers as (
+  SELECT * FROM {{ source('rds', 'customers')}} 
+), 
+companies as (
+  SELECT * FROM dev.stg_rds_companies
 ),
-
 renamed as (
-    select customer_id, country,
-    split_part(contact_name, ' ', 1) as first_name,
-    split_part(contact_name, ' ', 2) as last_name
-    from source
+    SELECT 
+    concat('rds-', customer_id) as customer_id, 
+    SPLIT_PART(contact_name, ' ', 1) as first_name,
+    SPLIT_PART(contact_name, ' ', -1) as last_name,
+    REPLACE(TRANSLATE(phone, '(,),-,.', ''), ' ', '') as updated_phone,
+    company_id
+    FROM customers 
+    JOIN companies ON companies.company_name = customers.company_name 
+), final as 
+ (SELECT
+   customer_id,
+   first_name,
+   last_name,
+   CASE WHEN LENGTH(updated_phone) = 10 THEN
+       '(' || SUBSTRING(updated_phone, 1, 3) || ') ' || 
+       SUBSTRING(updated_phone, 4, 3) || '-' ||
+       SUBSTRING(updated_phone, 7, 4) 
+       END as phone,
+   company_id
+  FROM renamed
 )
-
-select * from renamed
+SELECT * FROM final
